053777cf74307c78101103e0c7942f2200fc800a
mahdi-shafee <82397556+mahdi-shafee@users.noreply.github.com>
2023-12-02 01:50:03 +0330

    Update pipeline_stable_diffusion_xl.py

--- src/diffusers/pipelines/stable_diffusion_xl/pipeline_stable_diffusion_xl.py
+++ src/diffusers/pipelines/stable_diffusion_xl/pipeline_stable_diffusion_xl.py
@@ -1154,7 +1154,7 @@ class StableDiffusionXLPipeline(
                 latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs, return_dict=False)[0]
                 print(latents.shape)
                 image = self.vae.decode(latents / self.vae.config.scaling_factor, return_dict=False)[0]
-                print(type(image))
+                print(image.shape)
 
                 if callback_on_step_end is not None:
                     callback_kwargs = {}
